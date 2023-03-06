b03e3a0c530fa4e7917802d459cdcf9411b5463d
hrishii <hrishirajmc8547@gmail.com>
2023-03-07 00:55:06 +0530

    file updated

--- delete.sh
+++ delete.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-#program to delete repo branch from git bash one by one
-for branch in `git branch -r | grep -v HEAD`; do git push origin --delete ${branch#origin/}; done
+
+# This is a script that prints the squares of all numbers from 1 to a given number
+
+# Prompt the user to enter a number
+echo "Please enter a number:"
+read NUM
+
+# Calculate the squares of all numbers from 1 to the entered number
+for (( i=1; i<=$NUM; i++ ))
+do
+  SQUARE=$(( i*i ))
+  echo "The square of $i is: $SQUARE"
+done
 
