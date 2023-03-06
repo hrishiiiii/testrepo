d40388e7ba104352239be5ca7c63e80752ac728d
hrishii <hrishirajmc8547@gmail.com>
2023-03-07 00:21:16 +0530

    new shell script added

---
+++ shell1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a script that takes input from the user
+
+echo "Please enter your name: "
+read NAME
+
+echo "Please enter your age: "
+read AGE
+
+echo "Hello, $NAME! You are $AGE years old."
+
